78ea1c74aca784abd11432b699bc295ac5b5da5b
Danh <danh123098@gmail.com>
2024-04-07 13:53:21 +0700

    hiber

--- QuanLyThanhVien/src/main/java/GUI/MainGUI.java
+++ QuanLyThanhVien/src/main/java/GUI/MainGUI.java
@@ -7,6 +7,7 @@ package GUI;
 import GUI.ThanhVien.ThanhVienGUI;
 import GUI.ThietBi.ThietBiGUI;
 import GUI.ThongKeGUI.ThongKeGUI;
+import GUI.XuLyViPham.XuLyViPhamGUI;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -39,7 +40,7 @@ public class MainGUI extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         addComponent("Thành Viên",new ThanhVienGUI(),"Course.png");
         addComponent("Thiết Bị",new ThietBiGUI(),"Student.png");
-        addComponent("Xử Lý Vi Phạm",new ThanhVienGUI(),"Teacher.png");
+        addComponent("Xử Lý Vi Phạm",new XuLyViPhamGUI(),"Teacher.png");
         addComponent("Thống Kê",new ThongKeGUI(),"Pen.png");
         
         
